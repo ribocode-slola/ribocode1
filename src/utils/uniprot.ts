b7277c6d6c08745b57fc33d62631fb8dd9e46407
Andy Turner <A.G.D.Turner@gmail.com>
2026-07-23 19:05:50 +0100

    Fixed issue with e2e testing by modifing tsconfig.json to include Node type setup for e2e and playwright.config.ts

--- src/utils/uniprot.ts
+++ src/utils/uniprot.ts
@@ -1,7 +1,13 @@
 /**
  * UniProt API utilities for resolving gene names from UniProt accessions.
+ * 
+ * Copyright (c) 2024-now Ribocode contributors, licensed under MIT, See LICENSE file for more info.
+ * 
+ * @author Andy Turner <agdturner@gmail.com>
+ * @version 1.0.0
+ * @lastModified 2026-06-24
+ * @see https://github.com/ribocode-slola/ribocode1
  */
-
 export type UniProtGeneNameCache = Record<string, string | null>;
 
 type FetchLike = (input: RequestInfo | URL, init?: RequestInit) => Promise<Response>;
